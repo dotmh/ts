de742e3f9c7626c32901cfcef0f056f549fc7ed2
Martin <martin.haynes@focusrite.com>
2025-10-02 13:23:26 +0100

    chore: update gitignore to fix windows files

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,14 @@
+import {readFileSync} from 'node:fs';
+import {join} from 'node:path';
 import {configDefaults, defineConfig} from 'vitest/config';
+import {parse} from 'yaml';
+
+const WORKSPACE_FILE = 'pnpm-workspace.yaml';
 
 export default defineConfig({
   test: {
+    projects: parse(readFileSync(join(__dirname, WORKSPACE_FILE), 'utf8'))
+      .packages,
     exclude: [...configDefaults.exclude, '**/coverage/**', '**/dist/**'],
     coverage: {
       provider: 'istanbul',
--- vitest.workspace.ts
+++
@@ -1,8 +0,0 @@
-import {readFileSync} from 'node:fs';
-import {join} from 'node:path';
-import {parse} from 'yaml';
-
-const WORKSPACE_FILE = 'pnpm-workspace.yaml';
-
-export default parse(readFileSync(join(__dirname, WORKSPACE_FILE), 'utf8'))
-  .packages;
